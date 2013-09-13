c9662e47b7bdb395e1505077c244de941c8a7c73
ellmo <ellmunzai@gmail.com>
2013-09-13 22:06:50 +0200

    map class now uses tile class objects

--- crawler/map.py
+++ crawler/map.py
@@ -1,8 +1,22 @@
 import pdb
+import tile
 
 from locals import *
 
 class Map(object):
   def __init__(self):
-    self.tiles = MAP_01
-    pass
+    self._tiles = []
+    for row in MAP_01:
+      _tile_row = []
+      for column in row:
+        _tile_to_append = tile.create_based_on_number(column)
+        _tile_row.append(_tile_to_append)
+      self._tiles.append(_tile_row)
+
+  @property
+  def tiles(self):
+      return self._tiles
+  @tiles.setter
+  def tiles(self, value):
+      self._tiles = value
+
