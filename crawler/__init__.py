72d2b9dfbac7d90369a3e1096deccc60f9339f30
ellmo <ellmunzai@gmail.com>
2013-09-17 01:33:06 +0200

    adding Thing class

--- crawler/__init__.py
+++ crawler/__init__.py
@@ -1,7 +1,8 @@
+import camera
 import locals
 import map
-import camera
 import renderer
-import texture
-import tile
 import sprite
+import texture
+import thing
+import tile
---
+++ crawler/thing.py
@@ -0,0 +1,20 @@
+import pdb
+
+class Thing(object):
+  def __init__(self, name, blocking=False, sprite=None):
+    self._name = name
+    self._blocking = blocking
+    self._sprite = sprite
+
+  @property
+  def name(self):
+    return self._name
+
+  @property
+  def blocking(self):
+    return self._blocking
+
+  @property
+  def sprite(self):
+    return self._sprite
+
