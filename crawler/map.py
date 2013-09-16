b2790c71b3da86e0681cff44c71155f13f693473
ellmo <ellmunzai@gmail.com>
2013-09-17 01:29:45 +0200

    Map now accepts and keeps start position

--- crawler/map.py
+++ crawler/map.py
@@ -6,9 +6,13 @@ from locals import *
 class Map(object):
   def __init__(self):
     self._tiles = []
+    self._items = []
+    self._start_position = None
     for row in MAP_01:
       _tile_row = []
       for column in row:
+        if (column is 5 and self._start_position is None):
+          self._start_position = (MAP_01.index(row), row.index(column))
         _tile_to_append = tile.create_based_on_number(column)
         _tile_row.append(_tile_to_append)
       self._tiles.append(_tile_row)
@@ -16,7 +20,8 @@ class Map(object):
   @property
   def tiles(self):
     return self._tiles
-  @tiles.setter
-  def tiles(self, value):
-    self._tiles = value
+
+  @property
+  def start_position(self):
+      return self._start_position
 
