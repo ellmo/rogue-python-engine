e136cd8cfb19440c5ebbdfe5dd85750345201acc
ellmo <ellmunzai@gmail.com>
2013-09-18 19:44:08 +0200

    adding a method for converting texture to a pixel-by-pixel table to Texture model

--- crawler/texture.py
+++ crawler/texture.py
@@ -1,4 +1,3 @@
-import pdb
 import pygame
 
 from locals import *
@@ -7,6 +6,8 @@ class Texture(object):
   def __init__(self, image_path='res/purp.png'):
     self._image_path = image_path
     self._image = pygame.image.load(image_path)
+    self._converted = None
+    self._converted_darkened = None
 
   @property
   def image(self):
@@ -16,8 +17,27 @@ class Texture(object):
   def image_path(self):
     return self._image_path
 
+  @property
+  def converted(self):
+    if self._converted is None:
+      self._converted = self.__convert_to_pixel_table()
+      self._converted_darkened = self.__convert_to_pixel_table(True)
+    return self._converted
 
+  @property
+  def converted_darkened(self):
+    if self._converted is None:
+      self._converted = self.__convert_to_pixel_table()
+      self._converted_darkened = self.__convert_to_pixel_table(True)
+    return self._converted_darkened
 
-
-  def convert(self):
-    self._image.convert()
+  def __convert_to_pixel_table(self, darken=False):
+    image = self._image
+    table = []
+    if darken:
+      image.set_alpha(192)
+    for i in range(image.get_width()):
+      s = pygame.Surface((1, image.get_height())).convert()
+      s.blit(image, (-i, 0))
+      table.append(s)
+    return table
