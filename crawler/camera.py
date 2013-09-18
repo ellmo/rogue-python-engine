68509c32135110c6fdf48a81121bf06c9b7164f7
ellmo <ellmunzai@gmail.com>
2013-09-18 19:16:23 +0200

    refactoring Camera class

--- crawler/camera.py
+++ crawler/camera.py
@@ -1,10 +1,7 @@
-import pdb
-import math
-
 class Camera(object):
   def __init__(self, position, dirx, diry, planex, planey):
-    self._y = float(position[0])
-    self._x = float(position[1])
+    self._x = int(position[0])
+    self._y = int(position[1])
     self._dirx = float(dirx)
     self._diry = float(diry)
     self._planex = float(planex)
@@ -13,11 +10,44 @@ class Camera(object):
   @property
   def x(self):
     return self._x
+  @x.setter
+  def x(self, value):
+    self._x = value
 
   @property
   def y(self):
     return self._y
+  @y.setter
+  def y(self, value):
+    self._y = value
 
+  @property
+  def dirx(self):
+    return self._dirx
+  @dirx.setter
+  def dirx(self, value):
+    self._dirx = value
+
+  @property
+  def diry(self):
+    return self._diry
+  @diry.setter
+  def diry(self, value):
+    self._diry = value
+
+  @property
+  def planex(self):
+    return self._planex
+  @planex.setter
+  def planex(self, value):
+    self._planex = value
+
+  @property
+  def planey(self):
+    return self._planey
+  @planey.setter
+  def planey(self, value):
+    self._planey = value
 
 
 
